cc62c045d6a05e83eed5a99890991a6c4a4669a3
Marzan-Bin-Rahman <mailhereformarzan@gmail.com>
2023-04-08 20:15:34 +0600

    string library

---
+++ js-missing-concepts/string-library-operators.js
@@ -0,0 +1,17 @@
+let text = 'joker is back'
+
+console.log(text.length)
+
+// length shows the length of a string
+
+console.log(text.charAt(3))
+
+// charAt will show which character is in the desired position index starting from 0
+
+console.log(text.charCodeAt(1))
+
+// charCodeAt shows the character code at desired position
+
+console.log(text.slice(2, 5))
+
+// slice will show the letter in 2 index and will show up to 5-3=2 letter
