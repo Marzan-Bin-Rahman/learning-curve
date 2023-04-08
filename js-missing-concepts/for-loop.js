1bb93ed4a563636087786ea424b29c9fb577ae00
Marzan-Bin-Rahman <mailhereformarzan@gmail.com>
2023-04-08 20:33:04 +0600

    for loop basic

---
+++ js-missing-concepts/for-loop.js
@@ -0,0 +1,24 @@
+for (i = 10; i >= 0; i--) {
+     // console.log(i)
+}
+
+// will count from 10 to 1
+
+for (i = 0; i <= 10; i += 2) {
+     // console.log(i)
+}
+
+// will show the even numbers in 0-10
+
+for (i = 1; i <= 10; i += 2) {
+     // console.log(i)
+}
+
+// will show to event numbers in 1-10
+
+let sum = 0
+
+for(i=0; i<11; i++){
+     sum+=i
+}
+console.log(sum)
