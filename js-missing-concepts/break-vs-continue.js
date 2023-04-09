6ae5c11cd22f52887cd5ad3bfa9d8e422a691210
Marzan-Bin-Rahman <mailhereformarzan@gmail.com>
2023-04-09 17:47:59 +0600

    break and continue

---
+++ js-missing-concepts/break-vs-continue.js
@@ -0,0 +1,17 @@
+// for(i=0; i<=20; i++){
+//      if(i==10){
+//           break
+//      }
+//      console.log(i)
+// }
+
+// break will stop the loop for ever if the condition becomes true
+
+for(i=0; i<=20; i++){
+     if(i==10){
+          continue
+     }
+     console.log(i)
+}
+
+// continue will continue the loop but will not do the work if the condition gets true
