ed1f044779ca9eb10c96867e377d412de5a9f6ca
Marzan-Bin-Rahman <mailhereformarzan@gmail.com>
2023-04-09 17:43:32 +0600

    do while loop

---
+++ js-missing-concepts/do-while-loop.js
@@ -0,0 +1,15 @@
+let i = 20;
+do {
+     console.log(i);
+     i++;
+} while(i<= 10);
+
+// do while loop vs while loop
+
+/* 
+do while loop will first do the work then will go for the condition
+
+and while loop will first check condition and then will do work
+
+in the loop it will print although the the condition is not true
+*/
