2861415f1c9cf8a49d15fdf8c789544d9b112b21
Marzan-Bin-Rahman <mailhereformarzan@gmail.com>
2023-04-09 17:38:53 +0600

    while loop

---
+++ js-missing-concepts/while-loop.js
@@ -0,0 +1,25 @@
+// let i = 1
+// let sum = 0
+
+// while (i <= 10) {
+//      sum += i
+//      i++
+// }
+
+// console.log(sum)
+
+// this will give a sum of number from 1-10
+
+// let i = 0
+// let sum = 0
+
+// while (i <= 100) {
+//      if (i % 3 == 0 || i % 5 == 0) {
+//           sum += i
+//      }
+//      i++
+// }
+
+// console.log(sum)
+
+// this will give us the sum of the numbers in range of 1-100 which ar both divisable by 3 and 5
