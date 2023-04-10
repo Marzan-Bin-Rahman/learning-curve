9e6868e5c67f2211e0894979aada23fef9bb4424
Marzan-Bin-Rahman <mailhereformarzan@gmail.com>
2023-04-10 19:40:51 +0600

    map vs filter vs for-each

---
+++ js-missing-concepts/map-vs-filter-vs-for-each.js
@@ -0,0 +1,19 @@
+let nums = [1,2,43,4,56]
+// let sqrNums =[]
+// nums.forEach(num => {
+//      sqrNums.push(num*num)
+// });
+
+// console.log(sqrNums);
+
+// let sqrNums = nums.map((num)=>{
+//      return num*num;
+// })
+
+// console.log(sqrNums);
+
+// console.log(nums.filter((num)=>{
+//      return num<11
+// }));
+
+// for each will map elements and we have to push it to an array. map will make an array ownself and filter will give an array based on a conditon
