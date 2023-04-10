48856a6251d83554d2a9a0e51daee2c78183c1b6
Marzan-Bin-Rahman <mailhereformarzan@gmail.com>
2023-04-10 17:54:37 +0600

    hoisting-strict-mode

---
+++ js-missing-concepts/hoisting-strict-mode.js
@@ -0,0 +1,8 @@
+'use strict'
+
+// use strict inside quotation will prevent codes from not being secure
+
+x=20;
+console.log(x);
+
+// this portion will work if there is no strict mode and hoisting is mainly using a variable without declaring it and only for var
